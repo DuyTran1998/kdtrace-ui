d76cc515eb6186361511ceef4914539d03b06728
Duy Tran Ngoc <16521659@gm.uit.edu.vn>
2020-06-12 20:41:08 +0700

    add file gitignore

---
+++ src/components/Form-Login/Form-Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class FormLogin extends React.Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+export default FormLogin;
