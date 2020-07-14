43c3d8ba2bd626c138c444a73839f79c1982922f
t-le-cna <t-le@usen-next.jp>
2020-07-14 14:56:38 +0700

    change link 2

--- src/constants/API/api.js
+++ src/constants/API/api.js
@@ -1,75 +1,75 @@
-export const API_GET_USER_CONTEXT = "http://35.201.180.133:8080/api/getUserLogged";
+export const API_GET_USER_CONTEXT = "https://kdtrace.xyz/api/getUserLogged";
 
-export const API_LOGIN = "http://35.201.180.133:8080/api/login";
+export const API_LOGIN = "https://kdtrace.xyz/api/login";
 
-export const API_REGISTER = "http://35.201.180.133:8080/api/register";
-
-
-
-
-export const API_GET_ALL_USER = "http://35.201.180.133:8080/api/admin/getAllUsers";
-
-export const API_ACTIVE_USER_ACCOUNT = "http://35.201.180.133:8080/api/admin/active?user_id="
+export const API_REGISTER = "https://kdtrace.xyz/api/register";
 
 
 
 
-export const API_GET_PROFILE_PRODUCER = "http://35.201.180.133:8080/api/producer/get";
+export const API_GET_ALL_USER = "https://kdtrace.xyz/api/admin/getAllUsers";
 
-export const API_UPDATE_PROFILE_PRODUCER = "http://35.201.180.133:8080/api/producer/update";
-
-export const API_GET_ALL_PRODUCT = "http://35.201.180.133:8080/api/product/getAll";
-
-export const API_CREATE_PRODUCT = "http://35.201.180.133:8080/api/product/create";
-
-export const API_GET_PRODUCT_DETAIL = "http://35.201.180.133:8080/api/product/get?id=";
-
-export const API_ACCEPT_TO_SELL_TRANSACTION = "http://35.201.180.133:8080/api/process/acceptToSell?id=";
-
-export const API_REJECT_TO_SHELL_TRANSACTION = "http://35.201.180.133:8080/api/process/rejectToSell?id=";
+export const API_ACTIVE_USER_ACCOUNT = "https://kdtrace.xyz/api/admin/active?user_id="
 
 
-export const API_GET_ALL_TRANSPORT = "http://35.201.180.133:8080/api/transport/getAll";
-
-export const API_UPDATE_PROFILE_TRANSPORT = "http://35.201.180.133:8080/api/transport/update";
-
-export const  API_GET_PROFILE_TRANSPORT = "http://35.201.180.133:8080/api/transport/get";
-
-export const API_GET_ALL_CAR = "http://35.201.180.133:8080/api/transport/deliveryTruck/getAll";
-
-export const API_GET_ALL_CAR_AVAILABLE = "http://35.201.180.133:8080/api/transport/deliveryTruck/getAllAvailable";
-
-export const API_CREATE_CAR = "http://35.201.180.133:8080/api/transport/deliveryTruck/create";
-
-export const API_ACCEPT_TO_DELIVERY = "http://35.201.180.133:8080/api/process/acceptToDelivery?id=";
-
-export const API_REJECT_TO_DELIVERY = "http://35.201.180.133:8080/api/process/rejectToDelivery?id=";
-
-export const API_CONFIRM_TO_GET = "http://35.201.180.133:8080/api/process/confirmToGet?id=";
 
 
-export const API_GET_PROFILE_DISTRIBUTOR = "http://35.201.180.133:8080/api/distributor/get";
+export const API_GET_PROFILE_PRODUCER = "https://kdtrace.xyz/api/producer/get";
 
-export const API_UPDATE_PROFILE_DISTRIBUTOR = "http://35.201.180.133:8080/api/distributor/update";
+export const API_UPDATE_PROFILE_PRODUCER = "https://kdtrace.xyz/api/producer/update";
 
-export const API_GET_ALL_PRODUCT_FOR_DISTRIBUTOR = "http://35.201.180.133:8080/api/distributor/getAllProduct";
+export const API_GET_ALL_PRODUCT = "https://kdtrace.xyz/api/product/getAll";
+
+export const API_CREATE_PRODUCT = "https://kdtrace.xyz/api/product/create";
+
+export const API_GET_PRODUCT_DETAIL = "https://kdtrace.xyz/api/product/get?id=";
+
+export const API_ACCEPT_TO_SELL_TRANSACTION = "https://kdtrace.xyz/api/process/acceptToSell?id=";
+
+export const API_REJECT_TO_SHELL_TRANSACTION = "https://kdtrace.xyz/api/process/rejectToSell?id=";
 
 
-export const API_CREATE_TRANSACTION = "http://35.201.180.133:8080/api/process/create";
+export const API_GET_ALL_TRANSPORT = "https://kdtrace.xyz/api/transport/getAll";
 
-export const API_GET_ALL_TRANSACTION = "http://35.201.180.133:8080/api/process/getAllByDistributor";
+export const API_UPDATE_PROFILE_TRANSPORT = "https://kdtrace.xyz/api/transport/update";
 
-export const API_GET_TRANSACTION = "http://35.201.180.133:8080/api/process/get?id=";
+export const  API_GET_PROFILE_TRANSPORT = "https://kdtrace.xyz/api/transport/get";
 
-export const API_CHOOSE_TRANSPORT = "http://35.201.180.133:8080/api/process/chooseTransport?id="
+export const API_GET_ALL_CAR = "https://kdtrace.xyz/api/transport/deliveryTruck/getAll";
 
-export const API_CONFIRM_TO_RECEIPT= "http://35.201.180.133:8080/api/process/confirmToReceipt?id=";
+export const API_GET_ALL_CAR_AVAILABLE = "https://kdtrace.xyz/api/transport/deliveryTruck/getAllAvailable";
 
-export const API_GET_ALL_TRANSACTION_BY_PRODUCER = "http://35.201.180.133:8080/api/process/getAllByProducer";
+export const API_CREATE_CAR = "https://kdtrace.xyz/api/transport/deliveryTruck/create";
 
-export const API_GET_ALL_TRANSACTION_BY_TRANSPORT = "http://35.201.180.133:8080/api/process/getAllByTransport";
+export const API_ACCEPT_TO_DELIVERY = "https://kdtrace.xyz/api/process/acceptToDelivery?id=";
 
-export const API_DELETE_TRANSACTION = "http://35.201.180.133:8080/api/process?id=";
+export const API_REJECT_TO_DELIVERY = "https://kdtrace.xyz/api/process/rejectToDelivery?id=";
+
+export const API_CONFIRM_TO_GET = "https://kdtrace.xyz/api/process/confirmToGet?id=";
+
+
+export const API_GET_PROFILE_DISTRIBUTOR = "https://kdtrace.xyz/api/distributor/get";
+
+export const API_UPDATE_PROFILE_DISTRIBUTOR = "https://kdtrace.xyz/api/distributor/update";
+
+export const API_GET_ALL_PRODUCT_FOR_DISTRIBUTOR = "https://kdtrace.xyz/api/distributor/getAllProduct";
+
+
+export const API_CREATE_TRANSACTION = "https://kdtrace.xyz/api/process/create";
+
+export const API_GET_ALL_TRANSACTION = "https://kdtrace.xyz/api/process/getAllByDistributor";
+
+export const API_GET_TRANSACTION = "https://kdtrace.xyz/api/process/get?id=";
+
+export const API_CHOOSE_TRANSPORT = "https://kdtrace.xyz/api/process/chooseTransport?id="
+
+export const API_CONFIRM_TO_RECEIPT= "https://kdtrace.xyz/api/process/confirmToReceipt?id=";
+
+export const API_GET_ALL_TRANSACTION_BY_PRODUCER = "https://kdtrace.xyz/api/process/getAllByProducer";
+
+export const API_GET_ALL_TRANSACTION_BY_TRANSPORT = "https://kdtrace.xyz/api/process/getAllByTransport";
+
+export const API_DELETE_TRANSACTION = "https://kdtrace.xyz/api/process?id=";
 
 // -----------------------------------------------------------------------------------------------------------------------------
 
